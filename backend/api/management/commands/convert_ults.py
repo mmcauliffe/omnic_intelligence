785f2c9252c74be27d07dc390d6329d3b9f33695
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2019-09-03 11:51:59 -0600

    Fix ult states in conversion

--- backend/api/management/commands/convert_ults.py
+++ backend/api/management/commands/convert_ults.py
@@ -2,6 +2,42 @@ from django.core.management.base import BaseCommand, CommandError
 from backend.api import models
 
 
+def get_old_ult_states(self, round_object):
+    ultgains = self.ultgain_set.filter(round=round_object).all()
+    round_end = round(round_object.end - round_object.begin, 1)
+    if len(ultgains) == 0:
+        return [{'begin': 0, 'end': round_end, 'status': 'no_ult'}]
+
+    ultuses = self.ultuse_set.filter(round=round_object).all()
+    ultends = self.ultend_set.filter(round=round_object).all()
+    switches = self.switch_set.filter(round=round_object).all()
+    starts = sorted([round(x.time_point, 1) for x in ultgains])
+    switch_time_points = [round(x.time_point, 1) for x in switches]
+    ult_end_time_points = [round(x.time_point, 1) for x in ultends]
+    ends = sorted([round(x.time_point, 1) for x in ultuses] + switch_time_points)
+    segments = []
+    show_using_ults = round_object.game.match.event.spectator_mode == 'S'
+    for i, s in enumerate(starts):
+        if segments:
+            segments.append({'begin': segments[-1]['end'], 'end': s, 'status': 'no_ult'})
+        else:
+            segments.append({'begin': 0, 'end': s, 'status': 'no_ult'})
+        for e in ends:
+            if e >= s:
+                segments.append({'begin': s, 'end': e, 'status': 'has_ult'})
+                if e not in switch_time_points and ultends:
+                    for ue in ult_end_time_points:
+                        if ue >= e and (i == len(starts) - 1 or (ue >= s)):
+                            segments.append({'begin': e, 'end': ue, 'status': 'using_ult'})
+                            break
+                break
+        else:
+            segments.append({'begin': s, 'end': round_end, 'status': 'has_ult'})
+    if segments[-1]['end'] < round_end:
+        segments.append({'begin': segments[-1]['end'], 'end': round_end, 'status': 'no_ult'})
+    return segments
+
+
 class Command(BaseCommand):
     help = "Convert"
 
@@ -12,7 +48,7 @@ class Command(BaseCommand):
             ultimates = []
             for p in players:
                 print(p)
-                ult_states = p.get_ult_states(r)
+                ult_states = get_old_ult_states(p, r)
                 for j, u in enumerate(ult_states):
                     used_at = None
                     ended_at = None
